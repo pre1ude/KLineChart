aea25c6c6d103edd5b31d37faebd30114385c006
pre1ude <pre1ude@users.noreply.github.com>
2025-10-24 15:14:14 +0800

    build: adjust build script

--- scripts/config.js
+++ scripts/config.js
@@ -7,7 +7,7 @@ import terser from '@rollup/plugin-terser'
 import fileSize from 'rollup-plugin-filesize'
 import progress from 'rollup-plugin-progress'
 
-import { resolvePath, getVersion } from './utils.js' 
+import { resolvePath, getVersion } from './utils.js'
 
 const version = getVersion()
 
@@ -23,8 +23,9 @@ function createInputConfig ({ input, replaceValues }) {
   return {
     input,
     plugins: [
-      typescript(),
+      typescript(isDev ? { compilerOptions: {target: "es2018"}} : undefined),
       eslint({
+        fix: true,
         throwOnError: true
       }),
       nodeResolve(),
@@ -65,14 +66,14 @@ function createOutputConfig ({
      * Licensed under Apache License 2.0 https://www.apache.org/licenses/LICENSE-2.0
      */`.trim(),
   }
-  
+
   if (!!name) {
     config.name = name
   }
   return config
 }
 
-export { 
+export {
   createInputConfig,
   createOutputConfig,
   version,
