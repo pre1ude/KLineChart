27d55efeb6eb7f9b37d4b7e1d6a21f88fe1e0e20
pre1ude <pre1ude@users.noreply.github.com>
2025-11-17 17:22:44 +0800

    style(CandleZeroPriceLineView): improve line style

--- src/view/CandleZeroPriceLineView.ts
+++ src/view/CandleZeroPriceLineView.ts
@@ -16,15 +16,16 @@ import type DualYPane from '../pane/DualYPane'
 import { drawStaticFigure } from '../extension/figure'
 import View from './View'
 
-// todo draw the zero line for 0% percentage
-// 主轴一定是百分比轴
+/** 分时 0% 的线 */
 export default class CandleZeroPriceLineView extends View {
   override drawImp (ctx: CanvasRenderingContext2D): void {
     const widget = this.getWidget()
     const pane = widget.getPane()
     const bounding = widget.getBounding()
     const chartStore = pane.getChart().getChartStore()
-    const priceMarkStyles = chartStore.getStyles().candle.priceMark
+    const styles = chartStore.getStyles()
+    const gridStyles = styles.grid
+    const priceMarkStyles = styles.candle.priceMark
     const lastPriceMarkStyles = priceMarkStyles.last
     const lastPriceMarkLineStyles = lastPriceMarkStyles.line
     if (priceMarkStyles.show && lastPriceMarkStyles.show && lastPriceMarkLineStyles.show) {
@@ -43,9 +44,10 @@ export default class CandleZeroPriceLineView extends View {
           },
           styles: {
             style: lastPriceMarkLineStyles.style,
-            color: lastPriceMarkStyles.noChangeColor,
+            // color: lastPriceMarkStyles.noChangeColor,
+            color: '#b8cae665',
             size: lastPriceMarkLineStyles.size,
-            dashedValue: lastPriceMarkLineStyles.dashedValue
+            dashedValue: gridStyles.horizontal.dashedValue
           }
         })
       }
