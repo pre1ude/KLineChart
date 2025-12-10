bd7d7b3e61a3c44362c7738356c5a4f75279ac18
pre1ude <pre1ude@users.noreply.github.com>
2025-12-10 19:17:24 +0800

    feat(export): expose Crosshair type
    - Add Crosshair type export from common module
    - Make Crosshair type available to library consumers
    - Align with existing type export pattern for common types

--- src/index.ts
+++ src/index.ts
@@ -239,6 +239,7 @@ export type { Options } from './Options'
 export type { default as KLineData } from './common/KLineData'
 export type { default as Precision } from './common/Precision'
 export type { default as Point } from './common/Point'
+export type { default as Crosshair } from './common/Crosshair'
 
 // Style Types
 export type {
