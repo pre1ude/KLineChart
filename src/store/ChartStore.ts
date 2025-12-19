3e74cb1f49d1dd45b4f5a19d38666ed3719c3fbd
pre1ude <pre1ude@users.noreply.github.com>
2025-12-19 15:54:58 +0800

    fix(ChartStore): fix type

--- src/store/ChartStore.ts
+++ src/store/ChartStore.ts
@@ -419,7 +419,7 @@ export default class ChartStore {
             callback?.()
           }
         }
-        this._actionStore.execute(ActionType.OnDataReady)
+        this._actionStore.execute(ActionType.OnDataReady, undefined)
       } catch {}
     }
   }
