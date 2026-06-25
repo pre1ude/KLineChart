dd3333a63303ab577d4dd464b14b9f631402b83d
pre1ude <pre1ude@users.noreply.github.com>
2026-06-25 17:26:02 +0800

    Revert "style(widget): add top border to candle main container"
    This reverts commit 744936e8e46fa83106c7c59d29e3c4cec15edc87.

--- src/widget/CandleWidget.ts
+++ src/widget/CandleWidget.ts
@@ -1,5 +1,4 @@
 import type DualYPane from '../pane/DualYPane'
-import { createDom } from '../common/utils/dom'
 import MainWidget from './MainWidget'
 import {
   GridLayer,
@@ -14,7 +13,7 @@ export function CandleWidget(
   rootContainer: HTMLElement,
   pane: DualYPane
 ): MainWidget {
-  const widget = new MainWidget(rootContainer, pane, [
+  return new MainWidget(rootContainer, pane, [
     GridLayer,
     CandleLayer,
     IndicatorLayer,
@@ -22,15 +21,4 @@ export function CandleWidget(
     CrosshairLayer,
     CandleTooltipLayer
   ])
-  widget.getContainer().appendChild(createDom('div', {
-    position: 'absolute',
-    top: '0',
-    left: '0',
-    right: '0',
-    height: '1px',
-    backgroundColor: '#37465c',
-    zIndex: '3',
-    pointerEvents: 'none'
-  }))
-  return widget
 }
